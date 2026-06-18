9da9c26479476058dae50e7dbba0d02442bea2f3
peytoureaukevin <peytoureau.kevin@gmail.com>
2026-06-18 11:23:38 +0200

    Clean repository and update returns

--- Exercice11/main.py
+++ Exercice11/main.py
@@ -6,22 +6,24 @@ class BankAccount:
     def deposit(self, amount):
         if amount <= 0:
             print("Le montant du dépôt doit être positif.")
-            return
+            return False
 
         self.balance += amount
         print(f"Dépôt de {amount} effectué.")
+        return True
 
     def withdraw(self, amount):
         if amount <= 0:
             print("Le montant du retrait doit être positif.")
-            return
+            return False
 
         if amount > self.balance:
             print("Solde insuffisant.")
-            return
+            return False
 
         self.balance -= amount
         print(f"Retrait de {amount} effectué.")
+        return True
 
     def display_balance(self):
         print(f"Titulaire du compte : {self.account_holder}")
--- Exercice12/main.py
+++ Exercice12/main.py
@@ -12,32 +12,36 @@ class Library:
 
     def add_book(self, book):
         self.books.append(book)
+        return True
 
     def remove_book(self, book_title):
         for book in self.books:
             if book.title == book_title:
                 self.books.remove(book)
-                return
+                return True
 
         print(f"Le livre '{book_title}' n'est pas disponible dans la bibliothèque.")
+        return False
 
     def borrow_book(self, book_title):
         for book in self.books:
             if book.title == book_title:
                 self.books.remove(book)
                 self.borrowed_books_list.append(book)
-                return
+                return True
 
         print(f"Le livre '{book_title}' n'est pas disponible pour l'emprunt.")
+        return False
 
     def return_book(self, book_title):
         for book in self.borrowed_books_list:
             if book.title == book_title:
                 self.borrowed_books_list.remove(book)
                 self.books.append(book)
-                return
+                return True
 
         print(f"Le livre '{book_title}' n'a pas été emprunté.")
+        return False
 
     def available_books(self):
         return [book.title for book in self.books]
